c120011fad9efa760c6c32d4870501ec58bcd8c2
faisal <faisal2.rashid@live.uwe.ac.uk>
2025-10-29 11:19:17 +0000

    Update user_pet.gd

--- Scripts/user_pet.gd
+++ Scripts/user_pet.gd
@@ -24,9 +24,12 @@ func initialise() -> void:
 func on_event_ended(event: ScheduledActivity) -> void:
 	
 	if event.activity == "Walking":
-		pet_stats["Speed"] += event.result
+		pet_stats["Speed"] += process_walking(event)
 	
 	print("New stats:")
 	for stat in pet_stats.keys():
 		print(stat + " : " + str(pet_stats[stat]))
-	
+
+# Will return the result of the stat increase for walking
+func process_walking(event: ScheduledActivity) -> float:
+	return 0.5 * event.result
