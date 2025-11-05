e6e1f628880a9b4341a1676c17f9b58f07316375
Faisa-l <146775603+Faisa-l@users.noreply.github.com>
2025-11-05 18:41:44 +0000

    Add duration to event creator
    Can now specify how long the event lasts, in minutes

--- Scripts/displayed_activity_notification.gd
+++ Scripts/displayed_activity_notification.gd
@@ -10,8 +10,16 @@ var time_label : Label = $MarginContainer/HBoxContainer/Details/Time
 var title_label : Label = $MarginContainer/HBoxContainer/Tracking/Title
 
 @onready
+var ends_label : Label = $MarginContainer/HBoxContainer/Details/EndsAt
+
+@onready
 var activity_value_label : Label = $MarginContainer/HBoxContainer/Tracking/ValueDisplay
 
 func initialise(scheduled_activity : ScheduledActivity) -> void:
+	var start : Dictionary = scheduled_activity.starts
+	var ends : int = Time.get_unix_time_from_datetime_dict(start)
+	ends += (scheduled_activity.duration * 60)
+	
 	activity_label.text = scheduled_activity.activity
-	time_label.text = Time.get_datetime_string_from_datetime_dict(scheduled_activity.starts, true)
+	time_label.text = Time.get_datetime_string_from_datetime_dict(start, true)
+	ends_label.text = Time.get_datetime_string_from_unix_time(ends, true)
--- Scripts/event_creator.gd
+++ Scripts/event_creator.gd
@@ -6,10 +6,13 @@ signal event_submitted(hours: int, minutes: int, activity : String)
 var hours_box : SpinBox = $Container/HBoxContainer/Hours/HourEdit
 
 @onready
-var minutes_box : SpinBox= $Container/HBoxContainer/Minutes/MinuteEdit
+var minutes_box : SpinBox = $Container/HBoxContainer/Minutes/MinuteEdit
 
 @onready
 var activity_box : LineEdit = $Container/HBoxContainer/Activity/ActivityEdit
+
+@onready
+var duration_box : SpinBox = $Container/HBoxContainer/Duration/DurationEdit
 
 @onready
 var button : Button = $Container/Button
@@ -25,4 +28,4 @@ func _on_button_button_up() -> void:
 	if hours_box.value <= 0 or hours_box.value > 24: return
 	if minutes_box.value <= 0 or minutes_box.value > 60: return
 	
-	event_submitted.emit(hours_box.value, minutes_box.value, activity_box.text)
+	event_submitted.emit(hours_box.value, minutes_box.value, activity_box.text, duration_box.value)
