8905a8ee4e0e9acaa24a3c0c145111ba8bc226f1
Faisa-l <146775603+Faisa-l@users.noreply.github.com>
2025-11-05 19:50:48 +0000

    Remove unnecessary HBoxContainer in notification

--- Scripts/displayed_activity_notification.gd
+++ Scripts/displayed_activity_notification.gd
@@ -1,22 +1,22 @@
 class_name DisplayedActivityNotification extends PanelContainer
 
 @onready
-var activity_label : Label = $MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/Details/Activity
+var activity_label : Label = $MarginContainer/VBoxContainer/HBoxContainer/Details/Activity
 
 @onready
-var time_label : Label = $MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/Details/Time
+var time_label : Label = $MarginContainer/VBoxContainer/HBoxContainer/Details/Time
 
 @onready
-var title_label : Label = $MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/Tracking/Title
+var title_label : Label = $MarginContainer/VBoxContainer/HBoxContainer/Tracking/Title
 
 @onready
-var ends_label : Label = $MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/Details/EndsAt
+var ends_label : Label = $MarginContainer/VBoxContainer/HBoxContainer/Details/EndsAt
 
 @onready
-var name_label : Label = $MarginContainer/HBoxContainer/VBoxContainer/EventName
+var name_label : Label = $MarginContainer/VBoxContainer/EventName
 
 @onready
-var activity_value_label : Label = $MarginContainer/HBoxContainer/VBoxContainer/HBoxContainer/Tracking/ValueDisplay
+var activity_value_label : Label = $MarginContainer/VBoxContainer/HBoxContainer/Tracking/ValueDisplay
 
 func initialise(scheduled_activity : ScheduledActivity) -> void:
 	var start : Dictionary = scheduled_activity.starts
